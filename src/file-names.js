1e74e716c0e70c24be1f82192cf9fba102678055
s_iceman <s_iceman@protonmail.com>
2022-05-01 20:22:25 +0300

    feat: implement `file-names` and `simple chain` tasks

--- src/file-names.js
+++ src/file-names.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * There's a list of file, since two files cannot have equal names,
  * the one which comes later will have a suffix (k),
@@ -15,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  if (!names || names.length === 0){
+    return [];
+  }
+
+  let res = {};
+  names.forEach(n => {
+    if (res[n] === undefined){
+      res[n] = 1;
+    }
+    else {
+      ++res[n];
+      let newName = `${n}(${res[n] - 1})`;
+      res[newName] = 1;
+    }
+  })
+  return Object.keys(res);
 }
 
 module.exports = {
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,29 +1,48 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement chainMaker object according to task description
  * 
  */
+const SEP = '~~';
+const START = '( ';
+const END = ' )';
+
+
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  createLink(value){
+    return START + value + END;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    value = (value === undefined) ? '' : String(value);
+    this.chain.push(value);
+    return this;
   },
+
+  removeLink(position) {
+    if (!Number.isInteger(position) || position <= 0 || position >= this.chain.length){
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
+  },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let restChain = this.chain.map(e => this.createLink(e)).join(SEP);
+    this.chain = [];
+    return restChain;
   }
 };
 
