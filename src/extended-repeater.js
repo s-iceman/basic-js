7590d4beb0e96a3b168552fb629fa7fb261b5bb1
s_iceman <s_iceman@protonmail.com>
2022-05-01 16:17:00 +0300

    feat: implement some tasks for array

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create a repeating string based on the given parameters
  *  
@@ -15,10 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const SEPARATOR = '+';
+const ADD_SEPARATOR = '|';
+
+
+function repeater(str, options) {
+  if (typeof str !== 'string'){ str = String(str); }
+
+  const repeatTimes = (options.repeatTimes !== undefined) ? options.repeatTimes : 1;
+  const separator = (options.separator !== undefined) ? options.separator : SEPARATOR;
+  const tmpAddStr = options.addition;
+  const addStr = (tmpAddStr !== undefined) ? 
+      (typeof tmpAddStr == 'string' ? tmpAddStr : String(tmpAddStr)) :
+       '';
+  const tmpAddTimes = options.additionRepeatTimes;
+  let addRepeatTimes = (tmpAddTimes !== undefined) ? tmpAddTimes : 0;
+  if (addStr !== '' && addRepeatTimes === 0){
+    addRepeatTimes = 1;
+  }
+  
+  const addSeparator = (options.additionSeparator !== undefined) ? options.additionSeparator : ADD_SEPARATOR;
+  const fullAddSep = Array(addRepeatTimes).fill([addStr]).join(addSeparator);
+
+  return Array(repeatTimes).fill([str.concat(fullAddSep)]).join(separator);
 }
+
+const objWithSpecificCoercion = {
+  [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+};
 
 module.exports = {
   repeater
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create transformed array based on the control sequences that original
  * array contains
@@ -13,9 +11,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const MINUS_PREV = '--discard-prev';
+const PLUS_PREV = '--double-prev';
+const MINUS_NEXT = '--discard-next';
+const PLUS_NEXT = '--double-next';
+const FAKE_ELEM = 'fake';
+
+
+function transform(arr) {
+  if (!Array.isArray(arr)){
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let res = [];
+
+  for (let i = 0; i < arr.length; ++i){
+    let elem = arr[i];
+    if (elem == MINUS_PREV){
+      if (res.length !== 0){ res.pop(); }
+    }
+    else if (elem == PLUS_PREV){
+      if (res.length !== 0){ res.push(res[res.length - 1]); }
+    }
+    else if (elem == PLUS_NEXT){
+      if (i !== arr.length - 1){ res.push(arr[i + 1]); }
+    }
+    else if (elem == MINUS_NEXT){
+      res.push(FAKE_ELEM);
+      i += 1;
+    }
+    else { res.push(elem); }
+  }
+  return res.filter(e => e !== FAKE_ELEM);
 }
 
 module.exports = {
